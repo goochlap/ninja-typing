cdbe2aa91220a46c5eaf8419d45aab8a3202f9ca
anthony dupuis <anthonydupuis95@yahoo.fr>
2021-03-04 18:50:41 +0100

    avatar show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,6 +29,7 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { play } from "../game/ws";
 
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
+
 # Avatar.destroy_all
 # Board.destroy_all
+
+puts "Cleaning database..."
+Item.destroy_all
+
+puts "Creating items..."
+weapon_1 = { name: "sword", price: "30", image: "/image/weapon2.png" }
+weapon_2 =  { name: "silver sword", price: "50", image: "/image/weapon3.png" }
+weapon_3 =  { name: "golden sword", price: "70", image: "/image/weapon4.png" }
+weapon_4 =  { name: "rock sword", price: "100", image: "/image/weapon5.png" }
+
+
+[ weapon_1, weapon_2, weapon_3, weapon_4 ].each do |attributes|
+  item = Item.create!(attributes)
+  puts "Created #{item.name}"
+end
+puts "Finished!"
